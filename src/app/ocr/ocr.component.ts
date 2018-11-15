1cc7a66a97114fcb8aa6f56b072fcce9d3acfa41
ponbhanu <42290909+ponbhanu@users.noreply.github.com>
2018-11-15 09:57:55 +0530

    Update ocr.component.ts

--- src/app/ocr/ocr.component.ts
+++ src/app/ocr/ocr.component.ts
@@ -10,8 +10,16 @@ import { ToasterService } from 'angular2-toaster';
 })
 export class OcrComponent {
   taskId:any;
+  rfpInputId : any;
+  ocrStatusList: any = [{'status':'split','success':false},{'status':'ocr','success':false},{'status':'rfpmodel','success':false},{'status':'xml','success':false},{'status':'xls','success':false},{'status':'level11','success':false},{'status':'csv','success':false}];
+  mlStatusList: any = [{'status':'clauseId','success':false},{'status':'status','success':false}];
+ 
   constructor(public httpService:HttpService,public toasterService: ToasterService) {
-      this.asyncPredict();
+    this.rfpInputId =  localStorage.getItem('rfpId');
+    if (this.rfpInputId) {
+      this.split();
+    }
+  
     
   }
 
@@ -30,33 +38,103 @@ export class OcrComponent {
     };
   }
 
-  asyncPredict(){
+  split(){
     let formData: FormData = new FormData();
-    this.httpService.manageHttp('post','http://localhost:3000/api/asyncpredict', formData, getHeaders())
+    this.httpService.manageHttp('post','http://104.211.60.42:8080/rfp/ic/yankees/split/'+this.rfpInputId,'', getHeaders())
     .subscribe(response => {
       if (response.resultCode && response.resultCode === 'OK') {
-        //this.callLoader("task-bar");
-        this.httpService.manageHttp('get','http://localhost:3000/api/'+this.taskId+'/metrics','', getHeaders())
+        this.ocrStatusList[0].success = true;
+        this.httpService.manageHttp('post','http://104.211.60.42:8080/rfp/ic/yankees/ocr/'+this.rfpInputId,'', getHeaders())
         .subscribe(response => {
           if (response.resultCode && response.resultCode === 'OK') {
-            //this.callLoader("metrics-bar");
-          
-            this.httpService.manageHttp('get','http://localhost:3000/api/'+this.taskId+'/status','', getHeaders())
+            this.ocrStatusList[1].success = true;
+            this.httpService.manageHttp('post','http://104.211.60.42:8080/rfp/ic/yankees/model/'+this.rfpInputId,'', getHeaders())
             .subscribe(response => {
               if (response.resultCode && response.resultCode === 'OK') {
-                this.callLoader("ml-bar");
+                this.ocrStatusList[2].success = true;
+                this.httpService.manageHttp('post','http://104.211.60.42:8080/rfp/ic/yankees/xls/'+this.rfpInputId,'', getHeaders())
+                .subscribe(response => {
+                  if (response.resultCode && response.resultCode === 'OK') {
+                    this.ocrStatusList[3].success = true;
+                   this.httpService.manageHttp('post','http://104.211.60.42:8080/rfp/ic/yankees/csv/'+this.rfpInputId,'', getHeaders())
+                    .subscribe(response => {
+                      if (response.resultCode && response.resultCode === 'OK') {
+                        this.ocrStatusList[4].success = true;
+                        this.httpService.manageHttp('post','http://104.211.60.42:8080/rfp/ic/yankees/mv/'+this.rfpInputId,'', getHeaders())
+                        .subscribe(response => {
+                          if (response.resultCode && response.resultCode === 'OK') {
+                            this.ocrStatusList[5].success = true;
+                            this.callLoader("ocr-bar");
+                            this.asyncMlPredict();
+                          } else {
+                            this.ocrStatusList[5].success = false;
+                            this.toasterService.pop('error', 'Status failed at csv');
+                          }
+                        });
+                        
+                      } else {
+                        this.ocrStatusList[4].success = false;
+                        this.toasterService.pop('error', 'Status failed at xls');
+                      }
+                    });
+                    
+                  } else {
+                    this.ocrStatusList[3].success = false;
+                    this.toasterService.pop('error', 'Status failed at xml');
+                  }
+                });
               } else {
-                this.toasterService.pop('error', 'Status failed at CSV');
+                this.ocrStatusList[2].success = false;
+                this.toasterService.pop('error', 'Status failed at rfpModel');
               }
             });
         } else {
-          this.toasterService.pop('error', 'Status failed at RFP');
+          this.ocrStatusList[1].success = false;
+          this.toasterService.pop('error', 'Status failed at OCR');
         }
       });
       } else {
         this.taskId = '';
+        this.ocrStatusList[0].success = false;
         this.toasterService.pop('error', 'Status failed at getting Split');
       }
     });
   };
+
+
+
+
+
+  asyncMlPredict(){
+    let formData: FormData = new FormData();
+    this.httpService.manageHttp('post','http://localhost:3000/api/sendLotNumber/'+12,'', getHeaders())
+    .subscribe(response => {
+      if (response.resultCode && response.resultCode === 'OK') {
+        this.mlStatusList[0].success = true;
+        this.httpService.manageHttp('post','http://localhost:3000/api/sendLotNumber/'+12,'', getHeaders())
+        .subscribe(response => {
+          if (response.resultCode && response.resultCode === 'OK') {
+            this.mlStatusList[1].success = true;
+            this.httpService.manageHttp('post','http://localhost:3000/api/sendLotNumber/'+12,'', getHeaders())
+            .subscribe(response => {
+              if (response.resultCode && response.resultCode === 'OK') {
+                this.mlStatusList[2].success = true;
+                this.callLoader("ml-bar");
+              } else {
+                this.mlStatusList[0].success = true;
+                this.toasterService.pop('error', 'Status failed at Status');
+              }
+            });
+        } else {
+          this.mlStatusList[0].success = true;
+          this.toasterService.pop('error', 'Status failed at Metrics');
+        }
+      });
+      } else {
+        this.mlStatusList[0].success = false;
+        this.taskId = '';
+        this.toasterService.pop('error', 'Status failed at getting TaskId');
+      }
+    });
+  }
 }
